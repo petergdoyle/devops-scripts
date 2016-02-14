f848ea4de7cc1cb36dea4d52a66bb6db8b1dd2f2
petergdoyle <peter.g.doyle@gmail.com>
2016-02-14 09:12:55 -0700

    goofed previous commit. missed some mods

--- bash/centos/vagrant/perf-pack/autobench_latest_make_install.sh
+++ bash/centos/vagrant/perf-pack/autobench_latest_make_install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-eval 'autobench' > /dev/null 2>&1
-if [ $? -eq 127 ]; then
-  mkdir /usr/autobench
-  git clone https://github.com/menavaur/Autobench.git  /usr/autobench
-  cd /usr/autobench
-  make \
-  && make install
-  if [ $? -ne 0 ]; then
-    echo -e "\e[7;107;91mautobench - make did not build successfully. skipping.\e[0m"
+
+  eval "su - vagrant -c 'autobench'" > /dev/null 2>&1
+  if [ $? -eq 127 ]; then
+    git clone https://github.com/menavaur/Autobench.git  /tmp/autobench \
+    && cd /tmp/autobench \
+    && make \
+    && make install
+    if [ $? -ne 0 ]; then
+      display_error "\e[7;107;91mautobench  - make did not run successfully. skipping.\e[0m"
+    else
+      su - vagrant -c autobench \
+      && rm -fr /tmp/autobench
+    fi
   else
-    echo -e "\e[7;40;92mautobench - built successfully. skipping.\e[0m"
+    echo -e "\e[7;40;92mautobench already appears to be installed. skipping.\e[0m"
   fi
-else
-  echo -e "\e[7;40;92mautobench already appears to be downloaded. skipping.\e[0m"
-fi
